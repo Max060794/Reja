02bcb05a5e3139dba1974109dc455e860a20c63d
Max060794 <mironhanov@gmail.com>
2024-02-05 15:43:47 +0900

    File modul

---
+++ index.js
@@ -0,0 +1,83 @@
+// Module package: CORE
+
+
+// setTimeout(function () {
+//     console.log("ishga tushdi");
+// }, 5000);
+
+
+// console.log("*********************");
+
+
+// let number = 0;
+// setInterval(function () {
+//     console.log("hisob", number);
+//     number++;
+// }, 1000);
+
+
+// console.log("*********************");
+
+// const fs = require('fs');
+// const data = fs.readFileSync('./input.txt', 'utf8');
+// console.log(data);
+
+// console.log("*********************");
+
+// fs.writeFileSync("./input.txt", `${data} \n\t\t : Стив Джобс`);
+// const new_data = fs.readFileSync('./input.txt', 'utf8');
+// console.log(new_data);
+
+
+// Module package: EXTERNAL
+
+// const moment = require("moment");
+// const time = moment(). format("YYYY-MM-DD");
+// console.log(time);
+
+// // console.log("*********************");
+
+// const moment = require("moment");
+
+// setInterval(() => {
+//     const time = moment(). format();
+//     console.log(`hozirgi vaqt: ${time}`);
+// }, 5000);
+
+// console.log("*********************");
+
+// const inquirer = require("inquirer");
+// inquirer
+//     .prompt([{ type: "input", name:"raqam", message: "raqamni kiriting?" }])
+//     .then ((answer) => {
+//         console.log("man kiritgan raqam qiymati: ", answer.raqam);
+//     })
+//     .catch((err) => console.log(err));
+
+// console.log("*********************");
+
+// const validator = require("validator");
+// // const test = validator.isEmail("foo@bar.com");
+// const test = validator.isIP("66.249.74.45");
+// console.log("test:", test);
+
+// console.log("*********************");
+
+// const{ v4: uuiddv4 } = require('uuid');
+// const random = uuiddv4();
+// console.log("random:", random);
+
+// console.log("*********************");
+
+// const{ v4: uuidv4 } = require("uuid");
+// const random = uuidv4();
+// console.log("random:", random);
+
+// const chalk = require("chalk");
+// const log = console.log;
+
+// log(chalk.blue('Hello') + random + chalk.red('!'));
+
+// Module package: FILE
+
+

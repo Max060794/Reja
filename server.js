5d1b5dc58220e9f7da175ddfdcba88f227ceff2f
Max060794 <mironhanov@gmail.com>
2024-02-08 11:27:07 +0900

    Portfolio

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ console.log('Web Serverni boshlash');
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+const { log } = require("console");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data) 
+    }
+});
 
 //1: Kirish code
 app.use(express.static("public"));
@@ -15,6 +26,7 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4 Routing code
+
 // app.get("/hello", function(req, res) {
 //     res.end(`<h1 style="background: yellow;">HELLO WORLD by MAX</h1>`);
 // });
@@ -26,6 +38,10 @@ app.set("view engine", "ejs");
 app.post("/create-items", (req, res) => {
     console.log(req);
     res.json({ test: "success"});
+});
+
+app.get("/author", (req, res) => {
+    res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {

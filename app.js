ad2ee8aac0886cfb10d00d321e49fccfec8f8b0d
Max060794 <mironhanov@gmail.com>
2024-02-12 18:13:02 +0900

    MongoDB va CRUD operatsiyalari & MITASK-B

---
+++ app.js
@@ -0,0 +1,50 @@
+console.log('Web Serverni boshlash');
+const express = require("express");
+const app = express();
+
+// MongoDB choqirish
+const db = require("./server").db();
+
+//1: Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//2 Session code
+
+//3 Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+//4 Routing code
+app.post("/create-items", (req, res) => {
+    console.log("user entered /create-items");
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end("something went wrong");
+        } else {
+            res.end("successfully added");
+        }
+    });
+});
+
+app.get("/author", (req, res) => {
+    res.render("author", { user: user });
+});
+
+app.get("/", function (req, res) {
+    console.log("user entered /");
+    db.collection("plans").find().toArray((err, data) => {
+        if(err) {
+            console.log(err);
+            res.end("something went wrong");
+        } else {
+            console.log(data);
+            res.render("reja", { items: data });
+        }
+    });
+} );
+
+module.exports = app;
--- train.js
+++ train.js
@@ -1,100 +1,24 @@
-// // Callback functions:
+// Train Task-B
 
-// console.log("Jack Ma maslahatlari:");
-// const list = [
-//     "yahshi talaba boling", // 0-20
-//     "togri boshliq tanlang va koproq hato qiling", //20-30
-//     "uzingizga ishlashingizni boshlang", //30-40
-//     "siz kuchli bolgan narsalarni qiling", //40-50
-//     "yoshlarga investitsiya qiling", //50-60
-//     "endi dam oling, foydasi yoq endi", //60
-// ];
+// Shunday function tuzing, u 1ta string parametrga ega bolsin, 
+// hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
 
-// function maslahatBering(a, callback) {
-//     if (typeof a !== "number") callback("insert a number", null);
-//     else if (a <= 20) callback(null, list[0]);
-//     else if (a > 20 && a <= 30) callback(null, list[1]);
-//     else if (a > 30 && a <= 40) callback(null, list[2]);
-//     else if (a > 40 && a <= 50) callback(null, list[3]);
-//     else if (a > 50 && a <= 60) callback(null, list[4]);
-//     else {
-//         setTimeout(function () {
-//             callback(null, list[5]);
-//         },5000);
-//     }
-// }
+// MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
 
-// maslahatBering(65, (err, data) => {
-//     if(err) console.log("ERROR:", err);
-//     else {
-//     console.log("javob:", data);
-//     }
-// });
+function countNumber(string) {
+    let count = 0;
 
+    for (let i = 0; i < string.length; i++) {
+        
+        if (!isNaN(parseInt(string[i]))) {
+            count++;
+            }
+        }
+    return count;
+}
 
-// // Asynchronous functions:
-
-// console.log("Jack Ma maslahatlari:");
-// const list = [
-//     "yahshi talaba boling", // 0-20
-//     "togri boshliq tanlang va koproq hato qiling", //20-30
-//     "uzingizga ishlashingizni boshlang", //30-40
-//     "siz kuchli bolgan narsalarni qiling", //40-50
-//     "yoshlarga investitsiya qiling", //50-60
-//     "endi dam oling, foydasi yoq endi", //60
-// ];
-
-// async function maslahatBering(a) {
-//     if (typeof a !== "number") throw new Error("insert a number");
-//     else if (a <= 20) return list[0];
-//     else if (a > 20 && a <= 30) return list[1];
-//     else if (a > 30 && a <= 40) return list[2];
-//     else if (a > 40 && a <= 50) return list[3];
-//     else if (a > 50 && a <= 60) return list[4];
-//     else {
-//         return new Promise((resolve, reject) => {
-//             setTimeout(() => {
-//                 resolve(list[5]);
-//             }, 5000);
-//         });
-//     }
-// }
-
-// // then, catch
-// // console.log("passed here 0");
-// // maslahatBering(20)
-// //     .then((data) => {
-// //         maslahatBering(30)
-// //     .then((data) => {
-// //         maslahatBering(40)
-// //     .then((data) => {
-// //         console.log("javob:", data);
-// //     })
-// //     .catch((err) => {
-// //         console.log("ERROR:", err);
-// //     });
-// //         console.log("javob:", data);
-// //     })
-// //     .catch((err) => {
-// //         console.log("ERROR:", err);
-// //     });
-// //         console.log("javob:", data);
-// //     })
-// //     .catch((err) => {
-// //         console.log("ERROR:", err);
-// //     });
-// // console.log("passed here 1");
-
-// async function run() {
-//     let javob = await maslahatBering(25);
-//     console.log(javob);
-//     javob = await maslahatBering(70);
-//     console.log(javob);
-//     javob = await maslahatBering(41);
-//     console.log(javob);
-// }
-// run();
-
+const result = countNumber("ad2a54y79wet0sfgb9");
+console.log(result);
 
 
 // Train Task-A
@@ -103,14 +27,14 @@
 // hamda birinchi parametrdagi letterni ikkinchi parametrdagi 
 // sozdan qatnashga sonini return qilishi kerak boladi.
 
-function countLetter(letter, word) {
-    let count = 0;
-    for(let i=0; i<word.length; i++) {
-    if(letter === word[i]) {
-        count++
-    }
-    }
-    return count
-}
-const result = countLetter("e", "engineer")
-console.log(result)
+// function countLetter(letter, word) {
+//     let count = 0;
+//     for(let i=0; i<word.length; i++) {
+//     if(letter === word[i]) {
+//         count++
+//     }
+//     }
+//     return count
+// }
+// const result = countLetter("e", "engineer")
+// console.log(result)
